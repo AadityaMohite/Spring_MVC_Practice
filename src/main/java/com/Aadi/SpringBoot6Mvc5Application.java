0ef689541bb502f70cd422466bf5becd06966160
Aaditya_coder❤️ <mohiteaadi10@gmail.com>
2026-03-11 00:50:55 +0530

    Add files via upload

---
+++ src/main/java/com/Aadi/SpringBoot6Mvc5Application.java
@@ -0,0 +1,13 @@
+package com.Aadi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot6Mvc5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot6Mvc5Application.class, args);
+	}
+
+}
